1ebca11d4dc0d5572728f354b423f0e7e5dfc157
Javier Marin <javiermdleon95@gmail.com>
2024-06-01 22:26:22 -0600

    Se hicieron cambios en el modulo de permisos

--- src/app/demo/api/permisoRequest.ts
+++ src/app/demo/api/permisoRequest.ts
@@ -2,12 +2,12 @@ import {PermisoTrackingResponse} from "./permisoTrackingResponse";
 
 export class PermisoRequest{
 
-    idPermiso: string;
-    tipoPermiso: string;
+    idPermiso?: string;
+    tipoPermiso?: string;
     empleado: string;
     fechaInicio: string;
     fechaFinal: string;
-    descripcion: string;
-    permisoTracking: PermisoTrackingResponse[] = [];
+    descripcion?: string;
+    permisoTracking?: PermisoTrackingResponse[] = [];
 
 }
